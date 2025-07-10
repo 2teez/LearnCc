3bece07fc99d272703815764e3c7cd8500c216de
timothy adigun <adigun.timothy@gmail.com>
2025-07-10 12:45:25 +0100

    hello world cpp program

---
+++ beg++17/hello.cpp
@@ -0,0 +1,14 @@
+// hello.cpp
+
+// A complete C++ Program
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv)
+{
+
+    std::string name = "java";
+    std::cout << "Hello, World" << ", " << name << std::endl;
+
+    return 0;
+}
