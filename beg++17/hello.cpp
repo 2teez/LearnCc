a6b08a21a0115f962c0229258a6f6cde02105ccd
timothy adigun <adigun.timothy@gmail.com>
2025-07-10 14:12:24 +0100

    variable in c++

--- beg++17/hello.cpp
+++ beg++17/hello.cpp
@@ -8,7 +8,9 @@ int main(int argc, char** argv)
 {
 
     std::string name = "java";
+    int age = {32};
     std::cout << "Hello, World" << ", " << name << std::endl;
+    std::cout << "You are " << age << " years old." << std::endl;
 
     return 0;
 }
