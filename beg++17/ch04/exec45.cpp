9371d54d5fe13bd01085f3a44f92b33b738a7a8c
timothy adigun <adigun.timothy@gmail.com>
2025-07-29 03:04:29 +0100

    practice and play with smart pointer

---
+++ beg++17/ch04/exec45.cpp
@@ -0,0 +1,32 @@
+// exec43.cpp
+
+// A complete C++ Program
+#include <cstddef>
+#include <iostream>
+#include <cmath>
+#include <memory>
+
+int main(int argc, char** argv)
+{
+
+    std::cout << "Enter size of array: ";
+    size_t size {};
+    std::cin >> size;
+    // allocate and create a dynamic array
+    std::unique_ptr<double[]> arr {std::make_unique<double[]>(size)};
+
+    // initialize item on each index
+    for (size_t i = 0; i < size; ++i)
+    {
+        arr[i] = 1.0/std::pow((i+1.0), 2.0);
+    }
+    // calculate sum
+    double sum {};
+    for (size_t i = 0; i < size; ++i)
+    {
+        sum += arr[i];
+    }
+
+    std::cout << std::sqrt(sum * 6.0) << std::endl;
+    return 0;
+}
