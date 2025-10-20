815da2bef1794623de72bd78f857c2889a08df7e
timothy adigun <adigun.timothy@gmail.com>
2025-10-20 15:16:11 +0100

    hello world file in cpp

---
+++ practiced/hello.cpp
@@ -0,0 +1,14 @@
+// hello.cpp
+
+// A complete C++ Program
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv)
+{
+
+    std::string hello = "Hello, World!";
+    std::cout << hello << std::endl;
+
+    return 0;
+}
