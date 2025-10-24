a753115fbff5de3adeba7499f8981cd15a351749
timothy adigun <adigun.timothy@gmail.com>
2025-10-24 10:04:56 +0100

    cpp implementation

---
+++ practiced/queue.cpp
@@ -0,0 +1,15 @@
+// queque.cpp
+
+// A complete C++ Program
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv)
+{
+
+    std::string hello {"Hello, World!"};
+    std::cout << hello << std::endl;
+
+    return 0;
+}
+
