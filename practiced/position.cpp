104dc52e332069433c03528bff6120f43fbec264
timothy adigun <adigun.timothy@gmail.com>
2025-10-29 23:12:37 +0000

    still using smart pointers

--- practiced/position.cpp
+++ practiced/position.cpp
@@ -3,11 +3,22 @@
 // A complete C++ Program
 #include <iostream>
 #include <cmath>
+#include <memory>
 
 struct Point
 {
   public:
     explicit Point(float cx = 0, float cy = 0): x(cx), y(cy) {}
+    Point(const Point& pt): x(pt.x), y(pt.y) {}
+    Point& operator=(const Point& pt)
+    {
+        if (this != &pt)
+        {
+            x = pt.x;
+            y = pt.y;
+        }
+        return *this;
+    }
     virtual float distance() const = 0;
     virtual ~Point() = default;
   protected:
@@ -18,6 +29,7 @@ struct Point
 class Position: public Point {
     public:
         explicit Position(float cx = 0, float cy = 0): Point(cx, cy){}
+        explicit Position(const Point& pt): Point(pt) {}
         float distance() const override
         {
             return std::sqrt(std::pow(x, 2.0) + std::pow(y, 2.0));
@@ -30,5 +42,9 @@ int main(int argc, char** argv)
 
     Position pt(1.35f, 0.125f);
     std::cout << "Calculated distance: " << pt.distance() << "\n";
+    //
+    auto pt2 = std::make_unique<Position>(Position(3.1, 0.67));
+    std::cout << pt2->distance() << "\n";
+
     return 0;
 }
