44a252cdc5d21e49c0162f6e8a8f4ab2aaeefd29
timothy adigun <adigun.timothy@gmail.com>
2025-10-29 22:03:02 +0000

    classes still in cpp

---
+++ practiced/position.cpp
@@ -0,0 +1,34 @@
+// position.cpp
+
+// A complete C++ Program
+#include <iostream>
+#include <cmath>
+
+struct Point
+{
+  public:
+    explicit Point(float cx = 0, float cy = 0): x(cx), y(cy) {}
+    virtual float distance() const = 0;
+    virtual ~Point() = default;
+  protected:
+    float x;
+    float y;
+};
+
+class Position: public Point {
+    public:
+        explicit Position(float cx = 0, float cy = 0): Point(cx, cy){}
+        float distance() const override
+        {
+            return std::sqrt(std::pow(x, 2.0) + std::pow(y, 2.0));
+        }
+        ~Position() {}
+};
+
+int main(int argc, char** argv)
+{
+
+    Position pt(1.35, 0.125);
+    std::cout << "Calculated distance: " << pt.distance() << "\n";
+    return 0;
+}
