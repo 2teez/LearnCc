b6d87313ac75089d6a5cc9ce5cb40243222a4796
timothyadigun <adigun.timothy@gmail.com>
2025-11-07 08:28:30 +0000

    making copy constructor in cpp

---
+++ practiced/pract-copy.cpp
@@ -0,0 +1,35 @@
+// pract-copy.cpp
+
+// A complete C++ Program
+#include <iostream>
+#include <string>
+
+
+struct Person 
+{
+   Person(const std::string& nname = "", unsigned int aage = 0)
+	   : name(nname), age(aage) {}
+   Person(const Person& p): name(p.name), age(p.age) {}
+
+   ~Person() = default;
+
+   friend std::iostream& operator<<(std::iostream& os, const Person& p)
+   {
+	os << "Person[ Name: " << p.name << ", Age: "<< p.age << "]\n";
+	return os;
+   }
+
+   private:
+     std::string name;
+     unsigned int age;
+};
+
+
+int main(int argc, char** argv)
+{
+
+
+    auto java = Person{"java", 32};
+    return 0;
+}
+
