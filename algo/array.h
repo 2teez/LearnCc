cb0b64ac6c300896ce1439836cfc11c2cff7a005
timothy adigun <adigun.timothy@gmail.com>
2025-11-12 23:02:54 +0000

    header file in cpp

---
+++ algo/array.h
@@ -0,0 +1,98 @@
+
+#ifndef __ARRAY__
+#define __ARRAY__
+// include the structure of the header file
+#include <iostream>
+
+template <typename T>
+struct Array
+{
+    explicit Array(): size{10}, index{0}, _array(nullptr)
+    {
+        _array = new T[size]{};
+    }
+    explicit Array(size_t msize) : size{msize}, index{0}, _array(nullptr)
+    {
+        _array = new T[msize]{};
+    }
+
+    Array(const T* arr, size_t n): size{n}, index {size}
+    {
+        if (arr)
+        {
+            _array = new T[size]{};
+           for (size_t i = 0; i < size; i++)
+           {
+               _array[i] = arr[i];
+           }
+        }
+    }
+
+    Array(const Array<T>& arr): size {arr.size}, index {size}
+    {
+            _array = new T[arr.size]{};
+           for (size_t i = 0; i < arr.size; i++)
+           {
+               _array[i] = arr[i];
+           }
+    }
+    Array& operator=(const Array&) = default;
+
+    Array(Array&&) noexcept = default;
+
+    Array& operator=(Array&&) noexcept = default;
+
+    const T& operator[](size_t index) const
+    {
+        return _array[index];
+    }
+
+    constexpr bool push(const T& value)
+    {
+        if (index < size)
+        {
+            _array[index] = value;
+            index += 1;
+            return true;
+        }
+        return false;
+    }
+
+    [[nodiscard]]
+    Array& push(const T& value, size_t nindex)
+    {
+        if (nindex < size)
+        {
+            _array[nindex] = value;
+            return *this;
+        }
+        return *this;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Array<T>& arr)
+    {
+        if (arr.index == 0)
+        {
+            os << "Empty Array...";
+            return os;
+        }
+        os << "[";
+        for (size_t i = 0; i < arr.size-1; i++)
+        {
+            os << arr[i] <<", ";
+        }
+        os << arr[arr.size-1] << "]";
+        return os;
+    }
+
+    ~Array()
+    {
+        delete[] _array;
+    }
+    private:
+      size_t size;
+      size_t index;
+      T* _array;
+};
+
+#endif //__ARRAY__
