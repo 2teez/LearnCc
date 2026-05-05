35136e44dd6db3627abfa8f05dde2f0c7902a1e8
timothy adigun <adigun.timothy@gmail.com>
2026-05-05 23:10:31 +0100

    using brace initialization

--- crash_course/ch01/main.cpp
+++ crash_course/ch01/main.cpp
@@ -8,6 +8,15 @@ int main(int argc, char** argv)
 {
     Calculator calc(Operations::Add);
     std::cout << calc.calculate(5, 3) << std::endl;
+    //
+    Calculator calc2 {Operations::Subtract};
+    std::cout << calc2.calculate(5, 3) << std::endl;
+    //
+    Calculator calc3 {Operations::Multiply};
+    std::cout << calc3.calculate(5, 3) << std::endl;
+    //
+    Calculator calc4 {Operations::Divide};
+    std::cout << calc4.calculate(5, 3) << std::endl;
 
     return 0;
 }
