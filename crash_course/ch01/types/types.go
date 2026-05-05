6fba019d8e2a0f06833574a40def6bb6f7423eec
timothy adigun <adigun.timothy@gmail.com>
2026-05-05 20:59:05 +0100

    practice types and a type of enum in golang

--- crash_course/ch01/types/types.go
+++ crash_course/ch01/types/types.go
@@ -1,1 +1,31 @@
 package main
+
+const (
+	Add = iota
+	Subtract
+	Multiply
+	Divide
+)
+
+type Calculator struct {
+	op int
+}
+
+func (c *Calculator) Calculate(a, b int) int {
+	switch c.op {
+	case Add:
+		return a + b
+	case Subtract:
+		return a - b
+	case Multiply:
+		return a * b
+	case Divide:
+		return a / b
+	default:
+		return 0
+	}
+}
+
+func NewCalculator(op int) *Calculator {
+	return &Calculator{op: op}
+}
