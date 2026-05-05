a0bb25d5d87ed0486c9361afa23b138a41de0c72
timothy adigun <adigun.timothy@gmail.com>
2026-05-05 12:56:03 +0100

    added the implementation file

--- crash_course/ch00/step_function.cpp
+++ crash_course/ch00/step_function.cpp
@@ -1,6 +1,7 @@
 #include "step_function.hpp"
 
-int step_function(int x) {
+int step_function(int x)
+{
     int result = 0;
     if (x < 0)
     {
@@ -13,6 +14,20 @@ int step_function(int x) {
     else
     {
         result = 1;
+    }
+    return result;
+}
+
+int absolute_value(int x)
+{
+    int result = 0;
+    if (x < 0)
+    {
+        result = x * -1;
+    }
+    else
+    {
+        result = x;
     }
     return result;
 }
