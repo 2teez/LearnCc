5dab921a69bafb74665c180107cd92019f29d0f5
timothy adigun <adigun.timothy@gmail.com>
2026-05-05 12:57:32 +0100

    show output od absolute function

--- crash_course/ch00/step_function_main.cpp
+++ crash_course/ch00/step_function_main.cpp
@@ -19,5 +19,14 @@ int main(int argc, char** argv)
     std::cout << "step_function(" << num2 << ") = " << result2 << std::endl;
     std::cout << "step_function(" << num3 << ") = " << result3 << std::endl;
 
+    // checking absolute value function
+    int abs_result1 = absolute_value(num1);
+    int abs_result2 = absolute_value(num2);
+    int abs_result3 = absolute_value(num3);
+
+    std::cout << "absolute_value(" << num1 << ") = " << abs_result1 << std::endl;
+    std::cout << "absolute_value(" << num2 << ") = " << abs_result2 << std::endl;
+    std::cout << "absolute_value(" << num3 << ") = " << abs_result3 << std::endl;
+
     return 0;
 }
