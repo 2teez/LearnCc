5ef88bc9ccb49642fe7331970e12b9d89f223c88
timothy adigun <adigun.timothy@gmail.com>
2026-05-08 23:42:00 +0100

    corrected all the syntax for copy and assigment and move in obj in cpp

--- crash_course/ch02/timer.cpp
+++ crash_course/ch02/timer.cpp
@@ -8,9 +8,10 @@ struct Timestamp
 	Timestamp();
 	Timestamp(int, int, int);
 	Timestamp(const Timestamp&) = default;
-	Timestamp operator=(const Timestamp&) = default;
-	Timestamp& (Timestamp&&) noexcept = default;
+	Timestamp(Timestamp&&) noexcept = default;
+	Timestamp& operator=(const Timestamp&) = default;
 	Timestamp& operator=(Timestamp&&) noexcept = default;
+	friend std::ostream& operator<<(std::ostream&, const Timestamp&);
 	~Timestamp(){}
 
 	int hour;
@@ -23,8 +24,8 @@ struct Timer
 	Timer();
 	Timer(Timestamp timestamp);
 	Timer(const Timestamp&);
-	Timer& (Timestamp&&) noexcept;
-	Timer operator=(const Timestamp&);
+	Timer& operator=(const Timestamp&);
+	Timer(Timestamp&&) noexcept;
 	Timer& operator=(Timestamp&&) noexcept;
 	~Timer();
 
@@ -37,13 +38,20 @@ struct Timer
 int main(int argc, char** argv)
 {
 
+    Timestamp t1{}, t2{3, 4, 34};
+    std::cout << t1 << t2 << std::endl;
 
     return 0;
 }
 
-Timestamp::Timestamp(): hour{0}, minute{0}, second{0} 
+Timestamp::Timestamp(): hour{0}, minute{0}, second{0}
 {}
 
 Timestamp::Timestamp(int hr, int min, int sec): hour{hr}, minute{min}, second{sec}
 {}
 
+std::ostream& operator<<(std::ostream& os, const Timestamp& ts)
+{
+	os << ts.hour << ":" << ts.minute << ":" << ts.second << std::endl;
+	return os;
+}
