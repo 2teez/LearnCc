0392057be32156b937909eaba42d93a8a937b554
timothy adigun <adigun.timothy@gmail.com>
2026-05-08 23:23:15 +0100

    using cpp for timer cpp object

--- crash_course/ch02/timer.cpp
+++ crash_course/ch02/timer.cpp
@@ -2,14 +2,48 @@
 
 // A complete C++ Program
 #include <iostream>
-#include <string>
+
+struct Timestamp
+{
+	Timestamp();
+	Timestamp(int, int, int);
+	Timestamp(const Timestamp&) = default;
+	Timestamp operator=(const Timestamp&) = default;
+	Timestamp& (Timestamp&&) noexcept = default;
+	Timestamp& operator=(Timestamp&&) noexcept = default;
+	~Timestamp(){}
+
+	int hour;
+	int minute;
+	int second;
+};
+
+struct Timer
+{
+	Timer();
+	Timer(Timestamp timestamp);
+	Timer(const Timestamp&);
+	Timer& (Timestamp&&) noexcept;
+	Timer operator=(const Timestamp&);
+	Timer& operator=(Timestamp&&) noexcept;
+	~Timer();
+
+        friend std::ostream& operator<<(std::ostream&, const Timestamp&);
+
+	private:
+		Timestamp timestamp;
+};
 
 int main(int argc, char** argv)
 {
 
-    std::string hello {"Hello, World!"};
-    std::cout << hello << std::endl;
 
     return 0;
 }
+
+Timestamp::Timestamp(): hour{0}, minute{0}, second{0} 
+{}
+
+Timestamp::Timestamp(int hr, int min, int sec): hour{hr}, minute{min}, second{sec}
+{}
 
