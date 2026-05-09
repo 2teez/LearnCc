13fe24a029c67f247702d5d7e374c28785765725
timothy adigun <adigun.timothy@gmail.com>
2026-05-09 08:51:05 +0100

    using oop in cpp

---
+++ crash_course/ch03/logger4.cpp
@@ -0,0 +1,67 @@
+
+// A complete C++ Program
+#include <iostream>
+#include <iomanip>
+
+struct Logger
+{
+    virtual void log(long from, long to, double amount) const = 0;
+    virtual ~Logger() = default;
+};
+
+struct FileLogger : Logger
+{
+    void log(long from, long to, double amount) const override
+    {
+        std::cout << "[File] " << from << " -> " << to << ": "
+            << std::setw(5) << std::fixed << amount << "\n";
+    }
+};
+
+struct ConsoleLogger : Logger
+{
+    void log(long from, long to, double amount) const override
+    {
+        std::cout << "[Cons] " << from << " -> " << to << ": "
+            << std::setw(5) << std::fixed << amount << "\n";
+    }
+};
+
+struct Bank
+{
+    Bank(Logger& logger): m_logger{&logger}
+    {}
+
+    Bank(const Bank&) = delete;
+    Bank& operator=(const Bank&) = delete;
+    Bank(Bank&&) = delete;
+    Bank& operator=(Bank&&) = delete;
+    ~Bank() = default;
+
+    void set_logger(Logger& logger)
+    {
+        m_logger = &logger;
+    }
+
+    void make_transfer(long from, long to, double amount) const
+    {
+        m_logger->log(from, to, amount);
+    }
+
+private:
+    Logger* m_logger;
+};
+
+
+int main()
+{
+    ConsoleLogger console_logger {};
+    Bank bank{console_logger};
+    bank.make_transfer(1000, 2000, 49.95);
+    bank.make_transfer(2000, 4000, 20.00);
+    FileLogger file_logger {};
+    bank.set_logger(file_logger);
+    bank.make_transfer(3000, 2000, 75.00);
+
+    return 0;
+}
