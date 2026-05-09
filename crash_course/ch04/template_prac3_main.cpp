5813a1a94d1b4face10859339c73b6d2ea5935ea
timothy adigun <adigun.timothy@gmail.com>
2026-05-09 23:54:36 +0100

    main file

---
+++ crash_course/ch04/template_prac3_main.cpp
@@ -0,0 +1,12 @@
+
+// A complete C++ Program
+#include <iostream>
+#include "template_prac.hpp"
+
+int main()
+{
+    std::string hello {"Hello, World!"};
+    std::cout << hello << std::endl;
+
+    return 0;
+}
