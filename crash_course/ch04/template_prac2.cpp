2f844b1fac718443e0d46aa6211922a95ced5ba5
timothy adigun <adigun.timothy@gmail.com>
2026-05-09 21:16:48 +0100

    function decleration in cpp

--- crash_course/ch04/template_prac2.cpp
+++ crash_course/ch04/template_prac2.cpp
@@ -6,6 +6,23 @@
 int mode(const int*, std::size_t);
 
 template <std::integral T>
+T mode(const T*, std::size_t);
+
+int main()
+{
+    int arr[] {1, 2, 5, 3, 4, 5, 2, 6, 1, 2, 3, 2, 4, 5, 1, 2};
+    std::size_t size = std::size(arr);
+    std::cout << mode(arr, size) << std::endl;
+
+    // array of doubles
+    //double darr[] {1.5, 2.5, 3.5, 4.5, 5.5};
+    //std::size_t dsize = std::size(darr);
+    //std::cout << mode(darr, dsize) << std::endl;
+
+    return 0;
+}
+
+template <std::integral T>
 T mode(const T* arr, std::size_t length)
 {
     if (length == 0) return T{};
@@ -27,20 +44,6 @@ T mode(const T* arr, std::size_t length)
     }
     delete [] counter;
     return count;
-}
-
-int main()
-{
-    int arr[] {1, 2, 5, 3, 4, 5, 2, 6, 1, 2, 3, 2, 4, 5, 1, 2};
-    std::size_t size = std::size(arr);
-    std::cout << mode(arr, size) << std::endl;
-
-    // array of doubles
-    //double darr[] {1.5, 2.5, 3.5, 4.5, 5.5};
-    //std::size_t dsize = std::size(darr);
-    //std::cout << mode(darr, dsize) << std::endl;
-
-    return 0;
 }
 
 int mode(const int* arr, std::size_t length)
