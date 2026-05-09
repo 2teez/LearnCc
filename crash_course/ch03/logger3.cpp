537df6f0cebbe14d04717dcfde9b3301b229fdcf
timothy adigun <adigun.timothy@gmail.com>
2026-05-09 06:14:13 +0100

    logger file using derived and base class

--- crash_course/ch03/logger3.cpp
+++ crash_course/ch03/logger3.cpp
@@ -1,6 +1,7 @@
 
 // A complete C++ Program
 #include <iostream>
+#include <iomanip>
 
 struct Logger
 {
@@ -8,12 +9,58 @@ struct Logger
     virtual ~Logger() = default;
 };
 
+struct FileLogger : Logger
+{
+    void log(long from, long to, double amount) const override
+    {
+        std::cout << "[File] " << from << " -> " << to << ": "
+            << std::setw(5) << std::fixed << amount << "\n";
+    }
+};
+
+struct ConsoleLogger : Logger
+{
+    void log(long from, long to, double amount) const override
+    {
+        std::cout << "[Cons] " << from << " -> " << to << ": "
+            << std::setw(5) << std::fixed << amount << "\n";
+    }
+};
+
+struct Bank
+{
+    Bank(): m_logger{new ConsoleLogger{}}
+    {}
+
+    void set_logger(Logger* logger)
+    {
+        if (m_logger)
+        {
+            delete m_logger;
+        }
+        m_logger = logger;
+    }
+
+    void make_transfer(long from, long to, double amount)
+    {
+        if (m_logger)
+        {
+            m_logger->log(from, to, amount);
+        }
+    }
+
+private:
+    Logger* m_logger = nullptr;
+};
 
 
 int main()
 {
-    std::string hello {"Hello, World!"};
-    std::cout << hello << std::endl;
+    Bank bank;
+    bank.make_transfer(1000, 2000, 49.95);
+    bank.make_transfer(2000, 4000, 20.00);
+    bank.set_logger(new FileLogger{});
+    bank.make_transfer(3000, 2000, 75.00);
 
     return 0;
 }
