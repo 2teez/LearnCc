01fda582401fe77baf030c0199a85669626a4799
timothy adigun <adigun.timothy@gmail.com>
2026-05-09 09:45:35 +0100

    using data model and other cpp syntax

--- crash_course/ch03/logger4.cpp
+++ crash_course/ch03/logger4.cpp
@@ -20,11 +20,20 @@ struct FileLogger : Logger
 
 struct ConsoleLogger : Logger
 {
+    ConsoleLogger(const char* name): m_name{name} {}
+    ConsoleLogger(const ConsoleLogger& other) = delete;
+    ConsoleLogger(ConsoleLogger&& other) = delete;
+    ConsoleLogger& operator=(const ConsoleLogger& other) = delete;
+    ConsoleLogger& operator=(ConsoleLogger&& other) = delete;
+    ~ConsoleLogger() = default;
     void log(long from, long to, double amount) const override
     {
-        std::cout << "[Cons] " << from << " -> " << to << ": "
+        std::cout << m_name << " " << from << " -> " << to << ": "
             << std::setw(5) << std::fixed << amount << "\n";
     }
+
+private:
+    const char* m_name;
 };
 
 struct AccountDatabase
@@ -34,9 +43,80 @@ struct AccountDatabase
     virtual ~AccountDatabase() = default;
 };
 
+struct InMemoryDatabase : AccountDatabase
+{
+    InMemoryDatabase(int size): m_size{size}
+    {
+        m_data = new double[size]{};
+    }
+
+    InMemoryDatabase(const InMemoryDatabase& other): m_size{other.m_size}
+    {
+        m_data = new double[m_size]{};
+        for (int i = 0; i < m_size; ++i)
+        {
+            m_data[i] = other.m_data[i];
+        }
+    }
+
+    InMemoryDatabase(InMemoryDatabase&& other) noexcept: m_data{other.m_data}, m_size{other.m_size}
+    {
+        other.m_data = nullptr;
+        other.m_size = 0;
+    }
+
+    InMemoryDatabase& operator=(const InMemoryDatabase& other)
+    {
+        if (this != &other)
+        {
+            delete[] m_data;
+            m_size = other.m_size;
+            m_data = new double[m_size]{};
+            for (int i = 0; i < m_size; ++i)
+            {
+                m_data[i] = other.m_data[i];
+            }
+        }
+        return *this;
+    }
+
+    InMemoryDatabase& operator=(InMemoryDatabase&& other) noexcept
+    {
+        if (this != &other)
+        {
+            delete[] m_data;
+            m_size = other.m_size;
+            m_data = other.m_data;
+            other.m_data = nullptr;
+            other.m_size = 0;
+        }
+
+        return *this;
+    }
+
+    ~InMemoryDatabase()
+    {
+        delete[] m_data;
+    }
+
+    void set_amount(long id, double amount) override
+    {
+        m_data[id] = amount;
+    }
+
+    double get_amount(long id) const override
+    {
+        return m_data[id];
+    }
+
+private:
+    double* m_data;
+    int m_size;
+};
+
 struct Bank
 {
-    Bank(Logger& logger): m_logger{&logger}
+    Bank(Logger& logger, AccountDatabase& database): m_logger{&logger}, m_database{database}
     {}
 
     Bank(const Bank&) = delete;
@@ -53,17 +133,21 @@ struct Bank
     void make_transfer(long from, long to, double amount) const
     {
         m_logger->log(from, to, amount);
+        m_database.set_amount(from, m_database.get_amount(from) - amount);
+        m_database.set_amount(to, m_database.get_amount(to) + amount);
     }
 
 private:
     Logger* m_logger;
+    AccountDatabase& m_database;
 };
 
 
 int main()
 {
-    ConsoleLogger console_logger {};
-    Bank bank{console_logger};
+    ConsoleLogger console_logger {"[Cons]"};
+    InMemoryDatabase database {5};
+    Bank bank{console_logger, database };
     bank.make_transfer(1000, 2000, 49.95);
     bank.make_transfer(2000, 4000, 20.00);
     FileLogger file_logger {};
