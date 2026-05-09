7f179db7a32129d0b014c9df6e4f9c75de3ab61f
timothy adigun <adigun.timothy@gmail.com>
2026-05-10 00:21:44 +0100

    added double to mean

--- crash_course/ch04/template_prac3_main.cpp
+++ crash_course/ch04/template_prac3_main.cpp
@@ -10,5 +10,8 @@ int main()
     int arr[] = {1, 2, 3, 4, 5};
     std::cout << mean(arr) << std::endl;
 
+    double arr2[] = {1.5, 2.5, 3.5, 4.5, 5.5, 2.78, 9.23, 8.12};
+    std::cout << mean(arr2) << std::endl;
+
     return 0;
 }
