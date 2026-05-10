def7b9bd04783f6862c125e6d5403d5c213e9fa1
timothy adigun <adigun.timothy@gmail.com>
2026-05-10 16:32:14 +0100

    practice some function stuff in cpp

---
+++ crash_course/ch05/stat2.cpp
@@ -0,0 +1,13 @@
+
+// A complete C++ Program
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string hello {"Hello, World!"};
+    std::cout << hello << std::endl;
+
+    return 0;
+}
+            
