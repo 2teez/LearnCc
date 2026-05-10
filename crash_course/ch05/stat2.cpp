2dd3fed224453159e9eb2c5a16b0aef26d749551
timothy adigun <adigun.timothy@gmail.com>
2026-05-10 17:28:20 +0100

    using fold expression in cpp

--- crash_course/ch05/stat2.cpp
+++ crash_course/ch05/stat2.cpp
@@ -14,10 +14,20 @@ constexpr T sum(T t, Ts... ts)
     return t + sum(ts...);
 }
 
+// using fold expressions
+template <typename... Ts>
+constexpr auto sum_fold(Ts... ts)
+{
+    return (ts + ...);
+}
+
 int main()
 {
     const auto result = sum(1, 2, 3, 4, 5);
     std::cout << result << std::endl;
+
+    const auto result_fold = sum_fold(1, 2, 3, 4, 5);
+    std::cout << result_fold << std::endl;
 
     return 0;
 }
