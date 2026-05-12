3b4475ae9fde9c2b5cf4d7a0ef3fa1ab97df8414
timothy adigun <adigun.timothy@gmail.com>
2026-05-12 22:29:13 +0100

    playing with lambda in cpp

--- crash_course/ch05/function_obj.cpp
+++ crash_course/ch05/function_obj.cpp
@@ -18,5 +18,17 @@ int main()
     const auto result = operation(add, 5, 3);
     std::cout << result << std::endl;
 
+    const auto result2 = operation([](int a, int b) { return a - b; }, 5, 3);
+    std::cout << result2 << std::endl;
+
+    try {
+        const auto result3 = operation([](int a, int b) {
+            if ( b == 0) throw std::runtime_error("division by zero");
+            return a * b; }, 5, 3);
+        std::cout << result3 << std::endl;
+    } catch (const std::runtime_error& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
     return 0;
 }
