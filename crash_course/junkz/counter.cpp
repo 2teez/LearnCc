593ec91139b1ce84306213876b9b014193389672
timothy adigun <adigun.timothy@gmail.com>
2026-05-13 07:28:21 +0100

    practicing in cpp

--- crash_course/junkz/counter.cpp
+++ crash_course/junkz/counter.cpp
@@ -1,13 +1,46 @@
 
 // A complete C++ Program
 #include <iostream>
-#include <string>
+
+struct Counter
+{
+    explicit Counter(char letter);
+    void count_it(const char*);
+    void print() const;
+    private:
+        char letter;
+        int count;
+};
 
 int main()
 {
-    std::string hello {"Hello, World!"};
-    std::cout << hello << std::endl;
+    Counter counter{'l'};
+    counter.count_it("Hello, World!");
+    counter.print();
+
+    Counter counter2{'s'};
+    counter2.count_it("Sailor went to sea to see what he could see.");
+    counter2.print();
 
     return 0;
 }
-            
+
+Counter::Counter(char letter): letter{letter}, count{0}
+{
+}
+
+void Counter::count_it(const char* str)
+{
+    while(*str++)
+    {
+        if (*str == letter)
+        {
+            ++count;
+        }
+    }
+}
+
+void Counter::print() const
+{
+    std::cout << letter << ": " << count << std::endl;
+}
